8df29d58ee5768c9b55b6e9f3df16b9fdecc0ec2
Mark Altaweel <maltaweel@ucl.ac.uk>
2020-06-19 08:29:47 +0100

    commit

--- src/sentiment/sentiment_analysis.py
+++ src/sentiment/sentiment_analysis.py
@@ -3,4 +3,34 @@ Created on Jun 18, 2020
 
 @author: mark
 '''
+import os
+from os import listdir
 
+import csv
+
+class Sentiment:
+
+    def loadData(self):
+        pn=os.path.abspath(__file__)
+        pn=pn.split("src")[0]  
+        directory=os.path.join(pn,'modified')
+
+        try:
+            for f in listdir(directory):
+                rows=[]
+                with open(os.path.join(directory,f),'r') as csvfile:
+                    reader = csv.DictReader(csvfile)
+            
+                    for row in reader:
+                        text=row['Text']
+                        
+    
+        except IOError:
+            print ("Could not read file:", csvfile)
+    
+    def run(self):
+        self.loadData()
+
+if __name__ == '__main__':
+    s=Sentiment()
+    s.run()
