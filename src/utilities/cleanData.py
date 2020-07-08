fd9f5483d02827895aa4ffa470c4df3ed1c16ec2
Mark Altaweel <maltaweel@ucl.ac.uk>
2020-07-08 15:25:29 +0100

    commit

--- src/utilities/cleanData.py
+++ src/utilities/cleanData.py
@@ -10,44 +10,54 @@ from os import listdir
 
 import csv
 import re
-import nltk
 from nltk.corpus import stopwords 
 from nltk.tokenize import word_tokenize 
-from nltk.tokenize import RegexpTokenizer
-import fileinput
-
-
 
 #English stop words
 stop_words = set(stopwords.words('english')) 
 
 '''
-Main method to remove stop words, short (2-letter words or less), and clean text.
+Main method to remove stop and short (2-letter words or less) words, with the cleaning of text by removing symbols and non-language text.
 '''
 def cleanData():
+    #get the patheay to the data
     pn=os.path.abspath(__file__)
-    pn=pn.split("src")[0]  
+    pn=pn.split("src")[0]
+    
+    #the data directory
     directory=os.path.join(pn,'results')
+    
+    #output directory for cleaned file
     output_directory=os.path.join(pn,'modified')
+    
+    #pattern for removing text
     pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-                          
+                        
     files={}
+    
+    #now go through file(s) and begin cleaning text
     try:
         for f in listdir(directory):
             rows=[]
+            
+            #open files
             with open(os.path.join(directory,f),'r') as csvfile:
                 reader = csv.DictReader(csvfile)
             
-            
+                #go through the rows
                 for row in reader:
+                    
+                    #get text
                     text=row['Text']
                     
-                    
-                    
+                    #make text lower case 
                     text=text.lower()
+                    
+                    #initial processing 
                     text=re.sub('(\\b[A-Za-z] \\b|\\b [A-Za-z]\\b)', '', text)
                     text=pattern.sub("",text)
                     
+                    #tokenize text
                     word_tokens = word_tokenize(text) 
                     
                     #block of filters to clean sentences
@@ -91,16 +101,21 @@ def cleanData():
         print ("Could not read file:", csvfile)
 
 '''
-Method to output the cleaned data.
+Method to output the cleaned text data.
 @param fileOutput- the file output directory
 '''
 def output(data,fileOutput):
+    #the fieldnames or title of columns for the output file
     fieldnames = ['Datetime','ID','Link','Text','Username','Retweets','Hashtags','Geolocation']
+    
+    #output file
     with open(fileOutput, 'wt') as csvf:
         writer = csv.DictWriter(csvf, fieldnames=fieldnames)
 
+        #write the header
         writer.writeheader()  
         
+        #go through the data and print data with cleaned text
         for f in data:
             writer.writerow({'Datetime': str(f['Datetime']),
                              'ID':str(f['ID']),'Link':str(f['Link']),
